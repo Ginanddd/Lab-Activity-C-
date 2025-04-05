0c987282980a7e0298c1bdccca4909534a8d328f
Ginanddd <137371866+Ginanddd@users.noreply.github.com>
2025-04-05 14:28:05 +0800

    Add files via upload

---
+++ Villegas_SEMIFINALACTIVITY1/Villegas_SEMIFINALACTIVITY1/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Villegas_SEMIFINALACTIVITY1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblIdNumber = new System.Windows.Forms.Label();
+            this.txtIdNumber = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblMiddleName = new System.Windows.Forms.Label();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.lblYearLevel = new System.Windows.Forms.Label();
+            this.txtYearLevel = new System.Windows.Forms.TextBox();
+            this.lblBirthday = new System.Windows.Forms.Label();
+            this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblTitle.Location = new System.Drawing.Point(54, 9);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(348, 46);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Student Information";
+            // 
+            // lblIdNumber
+            // 
+            this.lblIdNumber.AutoSize = true;
+            this.lblIdNumber.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdNumber.Location = new System.Drawing.Point(19, 74);
+            this.lblIdNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIdNumber.Name = "lblIdNumber";
+            this.lblIdNumber.Size = new System.Drawing.Size(99, 23);
+            this.lblIdNumber.TabIndex = 1;
+            this.lblIdNumber.Text = "ID Number:";
+            // 
+            // txtIdNumber
+            // 
+            this.txtIdNumber.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdNumber.Location = new System.Drawing.Point(23, 101);
+            this.txtIdNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtIdNumber.Name = "txtIdNumber";
+            this.txtIdNumber.Size = new System.Drawing.Size(399, 30);
+            this.txtIdNumber.TabIndex = 2;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(19, 148);
+            this.lblFirstName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(96, 23);
+            this.lblFirstName.TabIndex = 3;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(23, 175);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(399, 30);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(19, 222);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(95, 23);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(23, 249);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(399, 30);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // lblMiddleName
+            // 
+            this.lblMiddleName.AutoSize = true;
+            this.lblMiddleName.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiddleName.Location = new System.Drawing.Point(19, 295);
+            this.lblMiddleName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMiddleName.Name = "lblMiddleName";
+            this.lblMiddleName.Size = new System.Drawing.Size(117, 23);
+            this.lblMiddleName.TabIndex = 7;
+            this.lblMiddleName.Text = "Middle Name:";
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddleName.Location = new System.Drawing.Point(23, 322);
+            this.txtMiddleName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(399, 30);
+            this.txtMiddleName.TabIndex = 8;
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCourse.Location = new System.Drawing.Point(19, 369);
+            this.lblCourse.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(67, 23);
+            this.lblCourse.TabIndex = 9;
+            this.lblCourse.Text = "Course:";
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCourse.Location = new System.Drawing.Point(23, 396);
+            this.txtCourse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(399, 30);
+            this.txtCourse.TabIndex = 10;
+            // 
+            // lblYearLevel
+            // 
+            this.lblYearLevel.AutoSize = true;
+            this.lblYearLevel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYearLevel.Location = new System.Drawing.Point(19, 443);
+            this.lblYearLevel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblYearLevel.Name = "lblYearLevel";
+            this.lblYearLevel.Size = new System.Drawing.Size(89, 23);
+            this.lblYearLevel.TabIndex = 11;
+            this.lblYearLevel.Text = "Year Level:";
+            // 
+            // txtYearLevel
+            // 
+            this.txtYearLevel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYearLevel.Location = new System.Drawing.Point(23, 470);
+            this.txtYearLevel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtYearLevel.Name = "txtYearLevel";
+            this.txtYearLevel.Size = new System.Drawing.Size(399, 30);
+            this.txtYearLevel.TabIndex = 12;
+            // 
+            // lblBirthday
+            // 
+            this.lblBirthday.AutoSize = true;
+            this.lblBirthday.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthday.Location = new System.Drawing.Point(19, 517);
+            this.lblBirthday.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBirthday.Name = "lblBirthday";
+            this.lblBirthday.Size = new System.Drawing.Size(77, 23);
+            this.lblBirthday.TabIndex = 13;
+            this.lblBirthday.Text = "Birthday:";
+            // 
+            // dtpBirthday
+            // 
+            this.dtpBirthday.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpBirthday.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBirthday.Location = new System.Drawing.Point(23, 544);
+            this.dtpBirthday.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpBirthday.Name = "dtpBirthday";
+            this.dtpBirthday.Size = new System.Drawing.Size(399, 30);
+            this.dtpBirthday.TabIndex = 14;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(23, 603);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(400, 43);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(445, 62);
+            this.panel1.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(445, 690);
+            this.panel2.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(445, 690);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dtpBirthday);
+            this.Controls.Add(this.lblBirthday);
+            this.Controls.Add(this.txtYearLevel);
+            this.Controls.Add(this.lblYearLevel);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.lblMiddleName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtIdNumber);
+            this.Controls.Add(this.lblIdNumber);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Information";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblIdNumber;
+        private System.Windows.Forms.TextBox txtIdNumber;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblMiddleName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.Label lblYearLevel;
+        private System.Windows.Forms.TextBox txtYearLevel;
+        private System.Windows.Forms.Label lblBirthday;
+        private System.Windows.Forms.DateTimePicker dtpBirthday;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Villegas_SEMIFINALACTIVITY1/Villegas_SEMIFINALACTIVITY1/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Villegas_SEMIFINALACTIVITY1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetupValidation();
+            SetupDateTimePicker();
+        }
+
+        private void SetupDateTimePicker()
+        {
+            dtpBirthday.Format = DateTimePickerFormat.Custom;
+            dtpBirthday.CustomFormat = "MM/dd/yyyy";
+            dtpBirthday.ShowUpDown = false;
+            dtpBirthday.Value = DateTime.Today;
+            dtpBirthday.MaxDate = DateTime.Today;
+            dtpBirthday.MinDate = new DateTime(1900, 1, 1);
+        }
+
+        private void SetupValidation()
+        {
+            // ID Number - numbers only
+            txtIdNumber.KeyPress += (sender, e) =>
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            };
+
+            // First Name - allow letters and numbers
+            txtFirstName.KeyPress += (sender, e) =>
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            };
+
+            // Last Name - allow letters and numbers
+            txtLastName.KeyPress += (sender, e) =>
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            };
+
+            // Middle Name - allow letters and numbers
+            txtMiddleName.KeyPress += (sender, e) =>
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            };
+
+            // Year Level - numbers only
+            txtYearLevel.KeyPress += (sender, e) =>
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            };
+
+            // Save button click event
+            btnSave.Click += BtnSave_Click;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                SaveStudentRecord();
+            }
+        }
+
+        private bool ValidateInputs()
+        {
+            if (string.IsNullOrWhiteSpace(txtIdNumber.Text))
+            {
+                MessageBox.Show("Please enter ID Number", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtIdNumber.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                MessageBox.Show("Please enter First Name", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtFirstName.Focus();
+                return false;
+            }
+
+            if (!Regex.IsMatch(txtFirstName.Text, @"^[a-zA-Z0-9]+$"))
+            {
+                MessageBox.Show("First Name should contain only letters and numbers", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtFirstName.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                MessageBox.Show("Please enter Last Name", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtLastName.Focus();
+                return false;
+            }
+
+            if (!Regex.IsMatch(txtLastName.Text, @"^[a-zA-Z0-9]+$"))
+            {
+                MessageBox.Show("Last Name should contain only letters and numbers", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtLastName.Focus();
+                return false;
+            }
+
+            if (!string.IsNullOrWhiteSpace(txtMiddleName.Text) && !Regex.IsMatch(txtMiddleName.Text, @"^[a-zA-Z0-9]+$"))
+            {
+                MessageBox.Show("Middle Name should contain only letters and numbers", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMiddleName.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtCourse.Text))
+            {
+                MessageBox.Show("Please enter Course", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtCourse.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtYearLevel.Text))
+            {
+                MessageBox.Show("Please enter Year Level", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtYearLevel.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SaveStudentRecord()
+        {
+            try
+            {
+                StringBuilder record = new StringBuilder();
+                record.AppendLine("ID Number: " + txtIdNumber.Text);
+                record.AppendLine("First Name: " + txtFirstName.Text);
+                record.AppendLine("Last Name: " + txtLastName.Text);
+                record.AppendLine("Middle Name: " + (String.IsNullOrWhiteSpace(txtMiddleName.Text) ? "N/A" : txtMiddleName.Text));
+                record.AppendLine("Course: " + txtCourse.Text);
+                record.AppendLine("Year Level: " + txtYearLevel.Text);
+                record.AppendLine("Birthday: " + dtpBirthday.Value.ToString("MM/dd/yyyy"));
+                record.AppendLine("----------------------------------------");
+
+                File.AppendAllText("student_record.txt", record.ToString());
+                MessageBox.Show("Student record saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving record: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtIdNumber.Clear();
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtMiddleName.Clear();
+            txtCourse.Clear();
+            txtYearLevel.Clear();
+            dtpBirthday.Value = DateTime.Today;
+            txtIdNumber.Focus();
+        }
+    }
+}
